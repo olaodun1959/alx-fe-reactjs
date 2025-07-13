01676b8232bf724750d609b65812bf9f13461ed6
oladimeji <oladimejiodunayo2017@gmail.com>
2025-07-13 03:09:43 +0100

    Add UserProfile component and integrate into App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
+import UserProfile from './components/UserProfile';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer'
 function App() {
@@ -8,8 +9,16 @@ function App() {
     <div>
       <WelcomeMessage />
       <Header />
+     
      <MainContent />
-       <Footer />
+      <div className="App">
+      <UserProfile 
+        name="Alice" 
+        age="25" 
+        bio="Loves hiking and photography" 
+      />
+    </div>
+      <Footer />
     </div>
   );
 }
